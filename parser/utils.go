6af2c56bba45293f39d99b13085bdd610315dd71
Roman Dolomanskyi <rdolomansky@users.noreply.github.com>
2025-01-06 00:29:46 +0200

    parser - in progress

--- parser/utils.go
+++ parser/utils.go
@@ -2,6 +2,6 @@ package parser
 
 import "github.com/gqlhub/gqlhub-core/token"
 
-func IsDescriptionToken(tok token.Type) bool {
+func IsStringValue(tok token.Type) bool {
 	return tok == token.STRING || tok == token.BLOCK_STRING
 }
